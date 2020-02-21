e69dbcd37843d94ad2d4cc95026960d522d9c553
italo yeltsin <br.yeltsin@gmail.com>
2020-02-21 11:06:52 +0000

    Login base

---
+++ src/login.ts
@@ -0,0 +1,18 @@
+const core = require('totalcross-core-dev');
+
+export class LoginHandler {
+    /**
+     * Check if token is valid
+     * return promise with true, if token is valid
+     */
+    async verifyToken() {
+        
+    } 
+
+    /**
+     * Show login an then password
+     */
+    async showLoginPanel() {
+        
+    }
+}
